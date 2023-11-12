86321d126273361d9aa1a630febd845a27cd8748
nazariipastukh <nazariy2002n@gmail.com>
2023-11-12 15:09:55 +0200

    final update commit

--- src/components/User/UserComponent.tsx
+++ src/components/User/UserComponent.tsx
@@ -1,12 +1,16 @@
 import styles from './User.module.css'
+import {useTheme} from "../../hooks";
 
 export const UserComponent = () => {
+    const {themeTrigger} = useTheme()
+
     return (
-        <section>
+        <section className={`${styles.userTheme} ${themeTrigger && styles.darkUser}`}>
             <a href={'https://github.com/nazariipastukh'} target="_blank" rel="noreferrer">
                 <img className={styles.user} src={'https://cdn-icons-png.flaticon.com/512/1946/1946429.png'}
                      alt={'user'}/>
             </a>
+            <p className={styles.name}>Nazarii</p>
         </section>
     );
 };
